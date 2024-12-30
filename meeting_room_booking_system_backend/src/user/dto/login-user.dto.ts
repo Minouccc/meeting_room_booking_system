44bd96e8d2f8464a6ffdae89b8eb9d35901bcdd3
Minouccc <343723899@qq.com>
2024-12-30 17:11:02 +0800

    chore: swagger

--- meeting_room_booking_system_backend/src/user/dto/login-user.dto.ts
+++ meeting_room_booking_system_backend/src/user/dto/login-user.dto.ts
@@ -1,11 +1,19 @@
 import { IsNotEmpty } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class LoginUserDto {
+  @ApiProperty({
+    description: '用户名',
+    example: 'lisi',
+  })
   @IsNotEmpty({
     message: '用户名不能为空',
   })
   username: string;
 
+  @ApiProperty({
+    description: '密码',
+    example: '333333',
+  })
   @IsNotEmpty({
     message: '密码不能为空',
   })
--- meeting_room_booking_system_backend/src/user/dto/register-user.dto.ts
+++ meeting_room_booking_system_backend/src/user/dto/register-user.dto.ts
@@ -1,16 +1,30 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterUserDto {
+  @ApiProperty({
+    description: '用户名',
+    example: 'lisi',
+  })
   @IsNotEmpty({
     message: '用户名不能为空',
   })
   username: string;
 
+  @ApiProperty({
+    description: '昵称',
+    example: '李四',
+  })
   @IsNotEmpty({
     message: '昵称不能为空',
   })
   nickName: string;
 
+  @ApiProperty({
+    description: '密码',
+    example: '333333',
+    minLength: 6,
+  })
   @IsNotEmpty({
     message: '密码不能为空',
   })
@@ -19,6 +33,10 @@ export class RegisterUserDto {
   })
   password: string;
 
+  @ApiProperty({
+    description: '邮箱',
+    example: 'xxx@xx.com',
+  })
   @IsNotEmpty({
     message: '邮箱不能为空',
   })
@@ -30,6 +48,10 @@ export class RegisterUserDto {
   )
   email: string;
 
+  @ApiProperty({
+    description: '验证码',
+    example: '123456',
+  })
   @IsNotEmpty({
     message: '验证码不能为空',
   })
