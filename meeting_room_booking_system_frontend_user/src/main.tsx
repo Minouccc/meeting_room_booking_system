94c5e8b1c51e1573a464fa0bb56d9a3fdd3c75ea
Minouccc <343723899@qq.com>
2024-12-30 21:28:05 +0800

    feat: add login and register page

---
+++ meeting_room_booking_system_frontend_user/src/main.tsx
@@ -0,0 +1,33 @@
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Register } from "./page/register";
+import { Login } from "./page/login";
+import { UpdatePassword } from "./page/UpdatePassword";
+import { ErrorPage } from "./page/error";
+
+const routes = [
+  {
+    path: "/",
+    element: <div>index</div>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "update_password",
+    element: <UpdatePassword />,
+  },
+];
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(<RouterProvider router={router} />);
