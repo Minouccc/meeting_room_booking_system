d736b4421201b75c6d18b67e7e52f9a30837c9ae
Minouccc <343723899@qq.com>
2024-12-30 14:43:51 +0800

    feat(server): login guard and permission

--- meeting_room_booking_system_backend/src/user/user.module.ts
+++ meeting_room_booking_system_backend/src/user/user.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { Role } from './entities/role.entity';
+import { Permission } from './entities/permission.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Role, Permission])],
   controllers: [UserController],
   providers: [UserService],
 })
